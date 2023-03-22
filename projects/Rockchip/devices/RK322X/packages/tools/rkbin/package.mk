9505ff0e0e055790dc26e27f802a8fc6334b97c6
ilmich <ardutu@gmail.com>
2023-03-22 16:09:26 +0100

    rk322x: initial device supports

---
+++ projects/Rockchip/devices/RK322X/packages/tools/rkbin/package.mk
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="rkbin"
+
+# Version is: knaerzche/tag:libreelec-991d0e4
+PKG_VERSION="7b83eae7baf1166672f4cc34db397c2665fdcbc0"
+PKG_SHA256="6ba07342d7d78c8be23f3b2745ee4d273449304b13ceb60a3df40a7ec4245dbc"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="nonfree"
+PKG_SITE="https://github.com/rockchip-linux/rkbin"
+PKG_URL="https://github.com/ilmich/rkbin/archive/$PKG_VERSION.tar.gz"
+PKG_LONGDESC="rkbin: Rockchip Firmware and Tool Binaries"
+PKG_TOOLCHAIN="manual"
+
